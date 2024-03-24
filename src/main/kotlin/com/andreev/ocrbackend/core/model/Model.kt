ff16af3026c9af8693d388f5fbc9e6f24d8719dd
G0P0Ke <m4n.of.future@gmail.com>
2024-03-24 21:37:52 +0300

    add configuration for all stages

--- src/main/kotlin/com/andreev/ocrbackend/core/model/Model.kt
+++ src/main/kotlin/com/andreev/ocrbackend/core/model/Model.kt
@@ -10,8 +10,6 @@ import javax.persistence.Convert
 import javax.persistence.Entity
 import javax.persistence.EntityListeners
 import javax.persistence.FetchType
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
